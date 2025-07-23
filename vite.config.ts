58221d47835cb6ad032566da642e08d7ca85c000
yanzhuang <canvascat@qq.com>
2025-07-23 23:12:23 +0800

    优化 vite.config.ts 文件的代码格式，调整导入顺序，增强可读性。新增 optimizeDeps 配置以支持 Rollup 选项，提升构建性能。

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,42 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import { fileURLToPath, URL } from 'node:url';
-import process from 'node:process';
-import tailwindcss from '@tailwindcss/vite'
+import process from "node:process";
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
-  resolve: {
-    // https://vitejs.dev/config/shared-options.html#resolve-alias
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
-  },
-  define: {
-    'process.env.IS_ELECTRON': JSON.stringify(process.env),
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  dev: {
-    sourcemap: true,
-  },
-  css: {
-    devSourcemap: true,
-  }
+	plugins: [vue(), vueJsx(), tailwindcss()],
+	resolve: {
+		// https://vitejs.dev/config/shared-options.html#resolve-alias
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+		extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+	},
+	define: {
+		"process.env.IS_ELECTRON": JSON.stringify(process.env),
+	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
+	dev: {
+		sourcemap: true,
+	},
+	css: {
+		devSourcemap: true,
+	},
+	optimizeDeps: {
+		rollupOptions: {
+			// https://github.com/vitejs/rolldown-vite/issues/304
+			jsx: "preserve",
+		},
+	},
 });
