c9cebeb5b4bf4f0d636aef969da388dc07b47922
yanzhuang <canvascat@qq.com>
2025-07-27 22:02:46 +0800

    新增 ESLint 和 Prettier 配置文件，更新 package.json 以添加相关依赖，提升代码质量和一致性。

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	singleQuote: false,
+	trailingComma: "all",
+	semi: true,
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	bracketSpacing: true,
+	arrowParens: "always",
+	endOfLine: "lf",
+};
+
+export default config;
