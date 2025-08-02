1c535be9000a4987442993ebab8db74af9a334eb
yanzhuang <canvascat@qq.com>
2025-08-02 16:30:27 +0800

    更新 .gitignore 文件以包含更多构建输出和缓存目录，修改 .npmrc 文件以启用 shamefully-hoist 和 node-linker，新增 Electron Forge 配置文件，添加 Vite 配置文件以支持主进程和预加载脚本，新增 Electron 主进程和预加载脚本，优化 Vite 配置以支持 Vue 开发工具，提升项目结构和可维护性。

--- vite.config.ts
+++ vite.config.ts
@@ -4,39 +4,41 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import process from "node:process";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), vueJsx(), tailwindcss()],
-	resolve: {
-		// https://vitejs.dev/config/shared-options.html#resolve-alias
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(() => {
+	return {
+		plugins: [vue(), vueJsx(), tailwindcss(), vueDevTools()],
+		resolve: {
+			// https://vitejs.dev/config/shared-options.html#resolve-alias
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+			extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
 		},
-		extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-	},
-	define: {
-		"process.env.IS_ELECTRON": JSON.stringify(process.env),
-	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
+		define: {
+			"process.env.IS_ELECTRON": JSON.stringify(process.env),
+		},
+
+		server: {
+			proxy: {
+				"/api": {
+					target: "http://localhost:3000",
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ""),
+				},
 			},
 		},
-	},
-	dev: {
-		sourcemap: true,
-	},
-	css: {
-		devSourcemap: true,
-	},
-	optimizeDeps: {
-		rollupOptions: {
-			// https://github.com/vitejs/rolldown-vite/issues/304
-			jsx: "preserve",
+		clearScreen: false,
+		dev: {
+			sourcemap: true,
 		},
-	},
+		optimizeDeps: {
+			rollupOptions: {
+				// https://github.com/vitejs/rolldown-vite/issues/304
+				jsx: "preserve",
+			},
+		},
+	};
 });
