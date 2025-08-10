e254bdad9e483448f0ee417f82f93793411e0a8b
yanzhuang <canvascat@qq.com>
2025-08-10 15:26:09 +0800

    重构 App.vue 组件，优化模板结构，新增 SlideUp 过渡组件，提升用户界面交互体验和代码可维护性。

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,8 @@
-<template>
-	<Navbar v-show="showNavbar" ref="navbar" />
-	<ScrollArea
-		as="main"
-		scroll-bar-class="top-[64px]! bottom-[64px]! h-[auto]!"
-		class="fixed! left-0 right-0 top-0 bottom-0"
-	>
-		<router-view v-slot="{ Component }">
-			<keep-alive :include="keepAliveComponents">
-				<component :is="Component" />
-			</keep-alive>
-		</router-view>
-	</ScrollArea>
-	<transition name="slide-up">
-		<Player v-if="enablePlayer" v-show="showPlayer" ref="player" />
-	</transition>
-
-	<ModalAddTrackToPlaylist v-if="isAccountLoggedIn" />
-	<ModalNewPlaylist v-if="isAccountLoggedIn" />
-	<transition v-if="enablePlayer" name="slide-up">
-		<Lyrics v-show="showLyrics" />
-	</transition>
-
-	<Toaster />
-</template>
-
 <script setup lang="ts">
 import { Toaster } from "@/components/ui/sonner";
 import "vue-sonner/style.css";
-import { computed, onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { computed, KeepAlive, onMounted } from "vue";
+import { RouterView, useRoute, useRouter } from "vue-router";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useGlobalStore } from "@/store/global";
 import * as auth from "@/utils/auth";
@@ -38,6 +12,7 @@ import Navbar from "./components/Navbar.vue";
 import Player from "./components/Player.vue";
 import Lyrics from "./views/lyrics.vue";
 import { useLikedStore } from "@/store/liked";
+import { SlideUp } from "./components/transition";
 
 const store = useGlobalStore();
 const router = useRouter();
@@ -83,25 +58,28 @@ onMounted(() => {
 });
 </script>
 
-<style>
-main > [data-reka-scroll-area-viewport] {
-	padding: 64px 10vw 96px 10vw;
-	box-sizing: border-box;
-}
+<template>
+	<Navbar v-show="showNavbar" />
+	<ScrollArea
+		as="main"
+		scroll-bar-class="top-[64px]! bottom-[64px]! h-[auto]!"
+		class="fixed! left-0 right-0 top-0 bottom-0 [&>[data-reka-scroll-area-viewport]]:pt-16 [&>[data-reka-scroll-area-viewport]]:pb-24 [&>[data-reka-scroll-area-viewport]]:px-[5vw] xl:[&>[data-reka-scroll-area-viewport]]:px-[10vw]"
+	>
+		<RouterView v-slot="{ Component }">
+			<KeepAlive :include="keepAliveComponents">
+				<component :is="Component" />
+			</KeepAlive>
+		</RouterView>
+	</ScrollArea>
+	<SlideUp>
+		<Player v-if="enablePlayer" v-show="showPlayer" ref="player" />
+	</SlideUp>
 
-@media (max-width: 1336px) {
-	main > [data-reka-scroll-area-viewport] {
-		padding: 64px 5vw 96px 5vw;
-	}
-}
+	<ModalAddTrackToPlaylist v-if="isAccountLoggedIn" />
+	<ModalNewPlaylist v-if="isAccountLoggedIn" />
+	<SlideUp v-if="enablePlayer">
+		<Lyrics v-show="showLyrics" />
+	</SlideUp>
 
-.slide-up-enter-active,
-.slide-up-leave-active {
-	transition: transform 0.4s;
-}
-
-.slide-up-enter-from,
-.slide-up-leave-to {
-	transform: translateY(100%);
-}
-</style>
+	<Toaster />
+</template>
