26771cb5dea7327d18433a43f3776a6e54ded5ec
yanzhuang <canvascat@qq.com>
2025-08-10 15:23:59 +0800

    新增 FadeIn 和 SlideUp 过渡组件，优化过渡效果，提升用户界面交互体验。

---
+++ src/components/transition/SlideUp.vue
@@ -0,0 +1,12 @@
+<template>
+	<transition
+		enter-from-class="translate-y-full"
+		enter-to-class="translate-y-0"
+		leave-from-class="translate-y-0"
+		leave-to-class="translate-y-full"
+		enter-active-class="transition-transform duration-300"
+		leave-active-class="transition-transform duration-300"
+	>
+		<slot />
+	</transition>
+</template>
---
+++ src/components/transition/index.ts
@@ -0,0 +1,2 @@
+export { default as SlideUp } from "./SlideUp.vue";
+export { default as FadeIn } from "./FadeIn.vue";
