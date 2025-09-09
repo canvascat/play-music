34bf7d4255faca7e7d7b1987373c7dbd119dbf3b
yanzhuang <canvascat@qq.com>
2025-09-09 22:07:02 +0800

    重构用户模块，修改fetchCookies导入路径为ncm-api/electron，简化代码结构。

--- electron/main/functions/ncm/user.ts
+++ electron/main/functions/ncm/user.ts
@@ -1,7 +1,7 @@
 import store from "@main/store";
 import { procedure } from "@main/tipc";
 import { BrowserWindow } from "electron";
-import { fetchCookies } from "@main/ncm/cookie";
+import { fetchCookies } from "ncm-api/electron";
 import user_account from "ncm-api/module/user_account";
 
 export const account = procedure.handle(async () => {
