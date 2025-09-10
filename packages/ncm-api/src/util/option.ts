1c35921cff1c9baa74e253d74ab0a2bce33bb5b8
yanzhuang <canvascat@qq.com>
2025-09-10 22:13:15 +0800

    重构请求选项接口，合并cookie类型，新增data属性，优化createOption函数以支持参数对象，提升代码可读性和灵活性。

--- packages/ncm-api/src/util/option.ts
+++ packages/ncm-api/src/util/option.ts
@@ -1,39 +1,34 @@
 /**
  * 请求选项接口
  */
-export interface RequestOptions {
-	cookie?: Record<string, string>;
-	headers?: Record<string, string>;
-	ip?: string;
+export interface RequestOptions<T = Record<string, any>> {
 	crypto?: string;
+	cookie?: Record<string, string> | string;
+	headers?: Record<string, string>;
 	ua?: string;
-	proxy?: string;
+	// proxy?: string;
+	ip?: string;
 	realIP?: string;
 	e_r?: boolean | string;
+	data?: T;
 }
 
 /**
  * 查询参数接口
  */
-export interface QueryParams {
-	crypto?: string;
-	ua?: string;
-	proxy?: string;
-	realIP?: string;
-	e_r?: string;
-}
+export type RequestParams<T extends Record<string, any> = {}> = Omit<RequestOptions<T>, "data"> & T;
 
 /**
  * 创建请求选项
- * @param query - 查询参数对象
+ * @param params - 查询参数对象
  * @param crypto - 默认加密方式（可选）
  * @returns 请求选项对象
  */
-export const createOption = (crypto: string = ""): RequestOptions => {
-	return {
-		crypto: crypto || "",
-		ua: "",
-
-		e_r: undefined,
-	};
-};
+export function createOption<T extends Record<string, any> = {}>(
+	params: RequestParams<T>,
+	crypto?: string,
+): RequestOptions<T> {
+	const { crypto: _crypto, cookie, headers, ua = "", ip, realIP, e_r, ...rest } = params;
+	crypto = _crypto || crypto || "";
+	return { crypto, headers, cookie, ua, realIP, ip, e_r, data: rest as T };
+}
